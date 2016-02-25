407ea4c0a3a5b260d181520e786732f3bab9ea5d
Caleb Asher <caleb@lytemine.com>
2016-02-24 23:12:56 -0700

    adds styling to calculator

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 $(document).ready(function(){
-
+	var keys = [];
+	var keyString;
+	var operators =[];
 	var working = [];
 	var toEvaluate;
 	var answer;
 
 	$('.keys').click(function(){
 		working.push($(this).text());
+		keys.push($(this).text());
+		keyString = keys.join('').toString();
 		toEvaluate = (working.join('').toString());
-		$('#result').html('<h3>'+toEvaluate+'</h3>')		
+		$('#result').html('<p>'+keyString+'<p>')		
+	});
+
+	$('.operator').click(function(){
+		keys = [];
+		working.push($(this).text());
+		toEvaluate = (working.join('').toString());
+		$('#result').html('<p>'+$(this).text()+'<p>')		
 	});
 
 	$('#equals').click(function(){
 		answer = eval(toEvaluate);
-		$('#result').html('<h3>'+answer+'</h3>')
+		$('#result').html('<p>'+answer+'<p>')
 		working = [];
+		keys=[];
 	});
 
 	$('#aclear').click(function(){
 		$('#result').empty();
 		working = [];
+		keys=[];
 	});
 
 	$('#cclear').click(function(){
 		working.pop();
 		toEvaluate = (working.join('').toString());
-		$('#result').html('<h3>'+toEvaluate+'</h3>')
+		$('#result').html('<p>'+toEvaluate+'<p>')
 	});
 
 	$('#percent').click(function(){
-		
+		var keyLength = (keyString * 100).toString();
+		var noDec = keyString.replace(".", "");
+		var dec;
+		if(keyLength.length < 4) {
+			dec = ".0"+noDec;
+			toEvaluate = dec;
+		}
+		else {
+			dec = "."+noDec;
+			toEvaluate = dec;
+		}
+		answer = eval(toEvaluate);
+		$('#result').html('<p>'+answer+'<p>')
 	});
 
 });
